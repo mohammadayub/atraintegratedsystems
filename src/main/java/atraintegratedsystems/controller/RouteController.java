8ff7db0743ec64dc66255daad293e592c5c456bf
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2024-11-02 09:03:06 +0430

    Route Controller is added.

--- src/main/java/atraintegratedsystems/controller/RouteController.java
+++ src/main/java/atraintegratedsystems/controller/RouteController.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Layout {
+public class RouteController {
     @GetMapping("/main")
     public String Shared_layout()
     {
         return "Main";
+    }
+    @GetMapping("/Registration")
+    public String getRegistration(){
+        return "RegistrationForm";
+    }
+    @GetMapping("/index")
+    public String getIndex(){
+        return "index";
     }
 }
