8c7495a9bf242be73bd26c8706dc8782c32f3ae8
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2024-11-10 11:55:52 +0430

    Lists Data Finance Application Data.

--- src/main/java/atraintegratedsystems/licenses/service/LicenseApplicantFinanceService.java
+++ src/main/java/atraintegratedsystems/licenses/service/LicenseApplicantFinanceService.java
@@ -4,6 +4,7 @@ import atraintegratedsystems.licenses.model.LicenseApplicant;
 import atraintegratedsystems.licenses.repository.LicenseApplicantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public class LicenseApplicantFinanceService {
 
     @Autowired
     private LicenseApplicantRepository licenseApplicantRepository;
-
+    @Transactional
     public List<LicenseApplicant> getAllUnpaid(){
         return licenseApplicantRepository.findByPaymentStatusIsNull();
     }
