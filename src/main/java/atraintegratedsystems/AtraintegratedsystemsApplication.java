011565a065c93bbdea849befa95553fac176be66
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2024-11-12 09:23:31 +0430

    Remove some Extra Columns

--- src/main/java/atraintegratedsystems/AtraintegratedsystemsApplication.java
+++ src/main/java/atraintegratedsystems/AtraintegratedsystemsApplication.java
@@ -2,9 +2,10 @@ package atraintegratedsystems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AtraintegratedsystemsApplication {
+public class AtraintegratedsystemsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtraintegratedsystemsApplication.class, args);
