f6c176a12e89a7c255192c8ae365a04daf6551d9
Rahimullah <rahimullahhamidi859@gmail.com>
2024-11-17 09:50:50 +0430

    ADD Some Changes in License Approval Dto

--- src/main/java/atraintegratedsystems/licenses/dto/LicenseApprovalDTO.java
+++ src/main/java/atraintegratedsystems/licenses/dto/LicenseApprovalDTO.java
@@ -1,5 +1,6 @@
 package atraintegratedsystems.licenses.dto;
 
+import atraintegratedsystems.licenses.model.LicenseApplicant;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -14,9 +15,32 @@ public class LicenseApprovalDTO {
     private LocalDate approvalDate;
     private Long licenseTypeId; // Reference to LicenseType by ID
     private String currencyType;
-    private String financeType;
+    //private String financeType;
     private BigDecimal licenseFees;
-    private Long licenseApplicantId; // Reference to LicenseApplicant by ID
-    private String licenseApplicantName; // Optional, if you want a human-readable name for the applicant
-    private String approvedBy;
+    //private Long licenseApplicantId; // Reference to LicenseApplicant by ID
+    private String licensePaymentOffice;
+    //private String licenseApplicantName; // Optional, if you want a human-readable name for the applicant
+    //private String approvedBy;
+    private BigDecimal administrativeYearlyFees;
+    private String adminstrivateYearlyFeesPaymentOffice;
+    private BigDecimal guaranteeFees;
+    private String guaranteeFeesPaymentOffice;
+    private BigDecimal databaseYearlyMaintainanceFees;
+    private String databaseYearlyMaintainanceFeesPaymentOffice;
+    private LicenseApplicant licenseApplicant;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
