7f3057d3cc080bb900fe24b0ba83535225867675
Rahimullah <rahimullahhamidi859@gmail.com>
2024-11-21 08:41:13 +0430

    Make Change also add localization and other licenses textarea

--- src/main/java/atraintegratedsystems/licenses/dto/LicenseApplicantApprovalDTO.java
+++ src/main/java/atraintegratedsystems/licenses/dto/LicenseApplicantApprovalDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
 @Data
 public class LicenseApplicantApprovalDTO {
 
