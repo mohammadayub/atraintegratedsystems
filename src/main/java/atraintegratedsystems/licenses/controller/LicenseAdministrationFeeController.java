9e2bf5eb8a4feaddc214a999bc19b459b1d5d428
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2024-12-07 15:50:10 +0430

    Print Administrative Fees Form developed.

--- src/main/java/atraintegratedsystems/licenses/controller/LicenseAdministrationFeeController.java
+++ src/main/java/atraintegratedsystems/licenses/controller/LicenseAdministrationFeeController.java
@@ -1,6 +1,7 @@
 package atraintegratedsystems.licenses.controller;
 
 
+import atraintegratedsystems.licenses.dto.LicenseApprovalDTO;
 import atraintegratedsystems.licenses.model.LicenseApproval;
 import atraintegratedsystems.licenses.service.LicenseAdministrationFeeService;
 import atraintegratedsystems.licenses.service.LicenseTypeService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,5 +28,53 @@ public class LicenseAdministrationFeeController {
         List<LicenseApproval> profiles = licenseAdministrationFeeService.getAllApprovalApplicants();
         model.addAttribute("profiles", profiles);
         return "licenses/finance/license_finance/administration_fees/license_administration_fee_list";
+    }
+
+    @GetMapping("/licenses/finance/license_finance/administration_fees/license_administration_fee_list/add")
+    public String PaymentConfirmationAdd(Model model){
+        model.addAttribute("licenseApprovalDTO",new LicenseApprovalDTO());
+        model.addAttribute("licenseTypes", licenseTypeService.findAll());
+        return "licenses/finance/license_finance/administration_fees/license_administration_fee_payment_confirmation";
+    }
+
+
+    @GetMapping("/licenses/finance/license_finance/administration_fees/license_administration_fee_list/update/{id}")
+    public String UpdateMcitFee(@PathVariable Long id, Model model){
+        LicenseApproval licenseApproval = licenseAdministrationFeeService.findById(id);
+        LicenseApprovalDTO licenseApprovalDTO = new LicenseApprovalDTO();
+        // Map fields from licenseApplicant to licenseApplicantDTO
+        licenseApprovalDTO.setId(licenseApproval.getId());
+        licenseApprovalDTO.setApprovalId(licenseApproval.getApprovalId());
+        licenseApprovalDTO.setApplicantLicenseCompanyName(licenseApproval.getLicenseApplicant().getCompanyLicenseName());
+        licenseApprovalDTO.setLicenseTypeName(licenseApproval.getLicenseType().getName());
+        licenseApprovalDTO.setApprovalDate(licenseApproval.getApprovalDate());
+        licenseApprovalDTO.setBoardDecisionNumber(licenseApproval.getBoardDecisionNumber());
+        licenseApprovalDTO.setApprovalStatus(licenseApproval.getApprovalStatus());
+        licenseApprovalDTO.setCurrencyType(licenseApproval.getCurrencyType());
+        licenseApprovalDTO.setAdministrativeYearlyFees(licenseApproval.getAdministrativeYearlyFees());
+        model.addAttribute("licenseTypes", licenseTypeService.findAll());
+        model.addAttribute("licenseApprovalDTO", licenseApprovalDTO);
+        return "licenses/finance/license_finance/administration_fees/license_administration_fee_payment_confirmation";
+
+    }
+
+
+    @GetMapping("/licenses/finance/license_finance/administration_fees/license_administration_fee_list/print/{id}")
+    public String GetTariff(@PathVariable Long id, Model model){
+        LicenseApproval licenseApproval = licenseAdministrationFeeService.findById(id);
+        LicenseApprovalDTO licenseApprovalDTO = new LicenseApprovalDTO();
+        // Map fields from licenseApplicant to licenseApplicantDTO
+        licenseApprovalDTO.setId(licenseApproval.getId());
+        licenseApprovalDTO.setApprovalId(licenseApproval.getApprovalId());
+        licenseApprovalDTO.setApplicantLicenseCompanyName(licenseApproval.getLicenseApplicant().getCompanyLicenseName());
+        licenseApprovalDTO.setLicenseTypeName(licenseApproval.getLicenseType().getName());
+        licenseApprovalDTO.setApprovalDate(licenseApproval.getApprovalDate());
+        licenseApprovalDTO.setBoardDecisionNumber(licenseApproval.getBoardDecisionNumber());
+        licenseApprovalDTO.setApprovalStatus(licenseApproval.getApprovalStatus());
+        licenseApprovalDTO.setCurrencyType(licenseApproval.getCurrencyType());
+        licenseApprovalDTO.setAdministrativeYearlyFees(licenseApproval.getAdministrativeYearlyFees());
+        model.addAttribute("licenseTypes", licenseTypeService.findAll());
+        model.addAttribute("licenseApprovalDTO", licenseApprovalDTO);
+        return "licenses/finance/license_finance/administration_fees/license_administration_fee_print_tariff";
     }
 }
