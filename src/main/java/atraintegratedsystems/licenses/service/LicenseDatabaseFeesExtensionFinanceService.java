71d5206780120e638d9bd57230f28a8a921fccbe
Rahimullah <rahimullahhamidi859@gmail.com>
2025-01-01 14:38:40 +0430

    Update Database Fees Extence Finanace

--- src/main/java/atraintegratedsystems/licenses/service/LicenseDatabaseFeesExtensionFinanceService.java
+++ src/main/java/atraintegratedsystems/licenses/service/LicenseDatabaseFeesExtensionFinanceService.java
@@ -2,6 +2,7 @@ package atraintegratedsystems.licenses.service;
 
 import atraintegratedsystems.licenses.model.LicenseDatabaseFeesExtension;
 import atraintegratedsystems.licenses.repository.LicenseDatabaseFeesExtensionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 public class LicenseDatabaseFeesExtensionFinanceService {
+    @Autowired
     private LicenseDatabaseFeesExtensionRepository licenseDatabaseFeesExtensionRepository;
 
     @Transactional
