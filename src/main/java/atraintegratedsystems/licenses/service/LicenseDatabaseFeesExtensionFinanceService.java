868511dd4764ce157104894c5ebf21adf04d4fe0
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2025-01-01 10:26:46 +0430

    GetAll Database Fees Completed.

--- src/main/java/atraintegratedsystems/licenses/service/LicenseDatabaseFeesExtensionFinanceService.java
+++ src/main/java/atraintegratedsystems/licenses/service/LicenseDatabaseFeesExtensionFinanceService.java
@@ -2,6 +2,7 @@ package atraintegratedsystems.licenses.service;
 
 import atraintegratedsystems.licenses.model.LicenseDatabaseFeesExtension;
 import atraintegratedsystems.licenses.repository.LicenseDatabaseFeesExtensionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,21 +11,12 @@ import java.util.List;
 
 @Service
 public class LicenseDatabaseFeesExtensionFinanceService {
+    @Autowired
     private LicenseDatabaseFeesExtensionRepository licenseDatabaseFeesExtensionRepository;
 
     @Transactional
     public List<LicenseDatabaseFeesExtension> getAllDatabaseFeesExtension(){
-        return licenseDatabaseFeesExtensionRepository.findExtensionsWithStatusNotYes();
+        return licenseDatabaseFeesExtensionRepository.findExtensionsWithStatusYes();
     }
 
-    @Transactional
-    public LicenseDatabaseFeesExtension findById(Long id) {
-        return licenseDatabaseFeesExtensionRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("LicenseApproval not found with id: " + id));
-    }
-
-    @Transactional
-    public void save(LicenseDatabaseFeesExtension licenseAdminFeesExtension) {
-        licenseDatabaseFeesExtensionRepository.save(licenseAdminFeesExtension);
-    }
 }
