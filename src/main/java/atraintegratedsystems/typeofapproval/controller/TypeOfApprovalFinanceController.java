7aa4dd378e8931b9ebca64dbc952f198f81f4f22
Rahimullah <rahimullahhamidi859@gmail.com>
2025-07-13 14:37:19 +0430

    Some Translation is Remaining

--- src/main/java/atraintegratedsystems/typeofapproval/controller/TypeOfApprovalFinanceController.java
+++ src/main/java/atraintegratedsystems/typeofapproval/controller/TypeOfApprovalFinanceController.java
@@ -52,7 +52,7 @@ public class TypeOfApprovalFinanceController {
 
 
     @GetMapping("/typeofapprovals/finance/payment/adminfeeconfirmation/{id}")
-    public String listTypeOfApprovalApplicationFeeConfirmation(@PathVariable Long id, Model model) {
+    public String listTypeOfApprovalAdminFeeConfirmation(@PathVariable Long id, Model model) {
         Optional<TypeOfApprovalApplicant> profileOpt = typeOfApprovalApplicantService.getById(id);
 
         if (profileOpt.isPresent()) {
@@ -106,10 +106,116 @@ public class TypeOfApprovalFinanceController {
     }
 
 
+    @GetMapping("/typeofapprovals/finance/payment/applicationfeeconfirmation/{id}")
+    public String listTypeOfApprovalApplicationFeeConfirmation(@PathVariable Long id, Model model) {
+        Optional<TypeOfApprovalApplicant> profileOpt = typeOfApprovalApplicantService.getById(id);
+
+        if (profileOpt.isPresent()) {
+            TypeOfApprovalApplicant applicant = profileOpt.get();
+            // Map entity to DTO
+            TypeOfApprovalApplicantDTO dto = new TypeOfApprovalApplicantDTO();
+            dto.setId(applicant.getId());
+            dto.setApplicationFeeStatus(applicant.getApplicationFeeStatus());
+            dto.setApplicationFeeBankVoucherNo(applicant.getApplicationFeeBankVoucherNo());
+            dto.setApplicationFeeVoucherDate(applicant.getApplicationFeeVoucherDate());
+            dto.setApplicationFeeBankVoucherSubmissionDate(applicant.getApplicationFeeBankVoucherSubmissionDate());
 
 
+            model.addAttribute("profile", dto);  // pass DTO, not entity!
+            return "typeofapprovals/finance/payment/applicationfeeconfirmation";
+        } else {
+            return "redirect:/typeofapprovals/finance/applicationfeelist";
+        }
+    }
 
 
+    @PostMapping("/typeofapprovals/finance/payment/applicationfeeconfirmation/save")
+    public String paymentApplicationFee(@ModelAttribute TypeOfApprovalApplicantDTO typeOfApprovalApplicantDTO) {
+        // Fetch the existing entity from the database
+        TypeOfApprovalApplicant referApplicant = typeOfApprovalApplicantService.findById(typeOfApprovalApplicantDTO.getId());
+
+        referApplicant.setApplicationFeeBankVoucherNo(typeOfApprovalApplicantDTO.getApplicationFeeBankVoucherNo());
+
+        String[] parts=typeOfApprovalApplicantDTO.getApplicationFeeVoucherDateJalali().split("-");
+        int jYear = Integer.parseInt(parts[0]);
+        int jMonth = Integer.parseInt(parts[1]);
+        int jDay = Integer.parseInt(parts[2]);
+
+        PersianCalendarUtils converter = new PersianCalendarUtils();
+        LocalDate applicationVoucherDate = converter.jalaliToGregorian(jYear, jMonth, jDay);
+        referApplicant.setApplicationFeeVoucherDate(applicationVoucherDate);
+
+        String[] subParts = typeOfApprovalApplicantDTO.getApplicationFeeBankVoucherSubmissionDateJalali().split("-");
+        int jSubYear = Integer.parseInt(subParts[0]);
+        int jSubMonth = Integer.parseInt(subParts[1]);
+        int jSubDay = Integer.parseInt(subParts[2]);
+
+        LocalDate applicationSubmissionDate = converter.jalaliToGregorian(jSubYear, jSubMonth, jSubDay);
+        referApplicant.setApplicationFeeBankVoucherSubmissionDate(applicationSubmissionDate);
+
+
+        referApplicant.setApplicationFeeStatus(typeOfApprovalApplicantDTO.getApplicationFeeStatus());
+
+        // Save the updated entity
+        typeOfApprovalApplicantService.save(referApplicant);
+        return "redirect:/typeofapprovals/finance/applicationfeelist";
+    }
+
+
+    @GetMapping("/typeofapprovals/finance/payment/certificatefeeconfirmation/{id}")
+    public String listTypeOfApprovalCertificateFeeConfirmation(@PathVariable Long id, Model model) {
+        Optional<TypeOfApprovalApplicant> profileOpt = typeOfApprovalApplicantService.getById(id);
+
+        if (profileOpt.isPresent()) {
+            TypeOfApprovalApplicant applicant = profileOpt.get();
+            // Map entity to DTO
+            TypeOfApprovalApplicantDTO dto = new TypeOfApprovalApplicantDTO();
+            dto.setId(applicant.getId());
+            dto.setCertificateFeeStatus(applicant.getCertificateFeeStatus());
+            dto.setCertificateFeeBankVoucherNo(applicant.getCertificateFeeBankVoucherNo());
+            dto.setCertificateFeeVoucherDate(applicant.getCertificateFeeVoucherDate());
+            dto.setCertificateFeeBankVoucherSubmissionDate(applicant.getCertificateFeeBankVoucherSubmissionDate());
+
+
+            model.addAttribute("profile", dto);  // pass DTO, not entity!
+            return "typeofapprovals/finance/payment/certificatefeeconfirmation";
+        } else {
+            return "redirect:/typeofapprovals/finance/certificatefeelist";
+        }
+    }
+
+
+    @PostMapping("/typeofapprovals/finance/payment/certificatefeeconfirmation/save")
+    public String paymentCertificateFee(@ModelAttribute TypeOfApprovalApplicantDTO typeOfApprovalApplicantDTO) {
+        // Fetch the existing entity from the database
+        TypeOfApprovalApplicant referApplicant = typeOfApprovalApplicantService.findById(typeOfApprovalApplicantDTO.getId());
+
+        referApplicant.setCertificateFeeBankVoucherNo(typeOfApprovalApplicantDTO.getCertificateFeeBankVoucherNo());
+
+        String[] parts = typeOfApprovalApplicantDTO.getCertificateFeeVoucherDateJalali().split("-");
+        int jYear = Integer.parseInt(parts[0]);
+        int jMonth = Integer.parseInt(parts[1]);
+        int jDay = Integer.parseInt(parts[2]);
+
+        PersianCalendarUtils converter = new PersianCalendarUtils();
+        LocalDate certificateVoucherDate = converter.jalaliToGregorian(jYear, jMonth, jDay);
+        referApplicant.setCertificateFeeVoucherDate(certificateVoucherDate);
+
+        String[] subParts = typeOfApprovalApplicantDTO.getCertificateFeeBankVoucherSubmissionDateJalali().split("-");
+        int jSubYear = Integer.parseInt(subParts[0]);
+        int jSubMonth = Integer.parseInt(subParts[1]);
+        int jSubDay = Integer.parseInt(subParts[2]);
+
+        LocalDate certificateSubmissionDate = converter.jalaliToGregorian(jSubYear, jSubMonth, jSubDay);
+        referApplicant.setCertificateFeeBankVoucherSubmissionDate(certificateSubmissionDate);
+
+
+        referApplicant.setCertificateFeeStatus(typeOfApprovalApplicantDTO.getCertificateFeeStatus());
+
+        // Save the updated entity
+        typeOfApprovalApplicantService.save(referApplicant);
+        return "redirect:/typeofapprovals/finance/certificatefeelist";
+    }
 
 
 
